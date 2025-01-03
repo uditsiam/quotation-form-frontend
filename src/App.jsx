932028f6a70e95a658efd1c30a9003918177f102
uditsiam <uditsiam@gmail.com>
2025-01-03 11:44:16 +0700

    test error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import QuotationForm from './Components/QuotationForm'
 import 'antd/dist/reset.css';
 import QuotationForm1 from './Components/QuotationForm2'
 import QuotationForm3 from './Components/QuotationForm3'
-import Test from './Components/test'
+import Test from './Components/Test'
 import Test2 from './Components/Test2'
 
 function App() {
